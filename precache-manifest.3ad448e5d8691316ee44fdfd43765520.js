1b70810d26ee36fc92a3f9f357d6fd84c6865044
Zohaib <apricopt@gmail.com>
2020-05-20 21:28:07 +0500

    first

---
+++ precache-manifest.3ad448e5d8691316ee44fdfd43765520.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0153f680fd7ab72f94bbb166733339a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a2c912552858afeefaa5",
+    "url": "/static/css/2.5293b4cd.chunk.css"
+  },
+  {
+    "revision": "6427719d7ec2d130e5f3",
+    "url": "/static/css/main.ae22da04.chunk.css"
+  },
+  {
+    "revision": "a2c912552858afeefaa5",
+    "url": "/static/js/2.cf78804e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.cf78804e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6427719d7ec2d130e5f3",
+    "url": "/static/js/main.f93c3a60.chunk.js"
+  },
+  {
+    "revision": "0e35831b56fb2c044e12",
+    "url": "/static/js/runtime-main.98a2b520.js"
+  },
+  {
+    "revision": "3caa124e05aa3fbd845c9ed4a3294374",
+    "url": "/static/media/about.3caa124e.png"
+  },
+  {
+    "revision": "9dfdb8542e22a8f5fbdeb7292c385130",
+    "url": "/static/media/example.9dfdb854.jpg"
+  },
+  {
+    "revision": "c64c51d2af00740a00d52dcf80064726",
+    "url": "/static/media/loader.c64c51d2.gif"
+  },
+  {
+    "revision": "d29d8d203adde93ecdf5ebdd52e1102c",
+    "url": "/static/media/mainlogo.d29d8d20.png"
+  },
+  {
+    "revision": "87e0d99bcdf9308330d994fce91d4d0b",
+    "url": "/static/media/men.87e0d99b.jpg"
+  },
+  {
+    "revision": "1db510b6afa04db310ae566b7eaf1cda",
+    "url": "/static/media/model1.1db510b6.jpg"
+  },
+  {
+    "revision": "b605ea6ffe7562fa236fd0985956a430",
+    "url": "/static/media/photography.b605ea6f.png"
+  },
+  {
+    "revision": "9377d675dc82731327216993c1be369d",
+    "url": "/static/media/slide1.9377d675.jpg"
+  },
+  {
+    "revision": "39ddf6b410d0708fbca04bee5bf86aff",
+    "url": "/static/media/slide22.39ddf6b4.jpg"
+  },
+  {
+    "revision": "e91f699f57bb3f12be330d9e2fbf5b07",
+    "url": "/static/media/slide3.e91f699f.jpg"
+  }
+]);
